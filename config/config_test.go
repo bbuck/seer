7b0ffac287f064d2af02d74c97b7f45fbff93958
Brandon Buck <lordizuriel@gmail.com>
2015-04-30 16:22:37 -0500

    Alter config_test to cleanup after itself.

--- config/config_test.go
+++ config/config_test.go
@@ -1,11 +1,38 @@
 package config
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func TestLoadOrCreateWillCreateMissingFile(t *testing.T) {
+	// Setup
+	os.Remove(configFileName)
+
+	_, err := os.Open(configFileName)
+	if !os.IsNotExist(err) {
+		t.Errorf("Expected %s not to exist, got %s instead.", configFileName, err)
+	}
+
+	LoadOrCreate()
+
+	_, err = os.Open(configFileName)
+	if err != nil {
+		t.Errorf("Expected no error, instead received %s", err)
+	}
+
+	// Cleanup
+	os.Remove(configFileName)
+}
 
 func TestDefaultConfigAfterLoadingIsValid(t *testing.T) {
+	// Setup
 	LoadOrCreate()
 
 	if Get().DB.Host != "localhost:7474" {
 		t.Errorf("Expected [database] host = %q but found %q", "localhost:7474", Get().DB.Host)
 	}
+
+	// Cleanup
+	os.Remove(configFileName)
 }
